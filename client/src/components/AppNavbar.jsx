af6c6d829a11a73bfee776326957947aad7f8f32
RitikSahu90 <162353443+RitikSahu90@users.noreply.github.com>
2025-06-23 19:06:28 +0530

    Added sql links

--- client/src/components/AppNavbar.jsx
+++ client/src/components/AppNavbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Navbar, Container,Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const AppNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="\">
+        <Navbar.Brand as={Link} to="/">
           Welcome <strong>NoteUp</strong>
         </Navbar.Brand>
 
         <Nav className="ms-auto">
-          <Nav.Link  as={Link} to="/login">Login</Nav.Link>
+          <Nav.Link as={Link} to="/login">Login</Nav.Link>
           <Nav.Link as={Link} to="/register">Register</Nav.Link>
         </Nav>
       </Container>
